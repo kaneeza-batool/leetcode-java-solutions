b9d64825172d74ea7a006389b7c6fa307b91ca40
Kaneeza Batool <kaneezabatoolmemon@gmail.com>
2026-06-09 10:33:34 +0500

    Update Solution.java

--- Arrays/1732-Find-the-Highest-Altitude/Solution.java
+++ Arrays/1732-Find-the-Highest-Altitude/Solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int currentAltitude = 0;
-        int maxAltitude = 0;
-        for (int i=0; i<gain.length; i++) {
-            currentAltitude += gain[i];
-            maxAltitude = Math.max(currentAltitude, maxAltitude); 
+        int[] altitudes = new int[gain.length + 1];
+        int max = 0;
+
+        for (int i = 1; i < altitudes.length; i++) {
+            altitudes[i] = altitudes[i - 1] + gain[i - 1];
+            if (altitudes[i] > max) {
+                max = altitudes[i];
+            }
         }
 
-        return maxAltitude;
+        return max;
     }
 }
